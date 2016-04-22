fe64b39d6fa282c62749a7c869f7494d6ecc68ea
chuwilliamson <chuwilliamson@gmail.com>
2016-04-21 20:43:17 -0500

    clicking things
    and changing colors

--- root/astar.py
+++ root/astar.py
@@ -1,62 +1,97 @@
 import pygame
 import math
-class Node:
+white = (255,255,255)
+black = (0,0,0)
+class Node(object):
+	
 	def __init__(self, x, y):		
 		#MANUAL SIZE
-		#nxn
+		
 		SIZE = 50
 		self.width = SIZE
 		self.height = SIZE
 		
 		self.x = (5 + self.width) * x + 5
-		self.y = (5 + self.width) * y + 5
-		self.pos = (self.x, self.y)		
-		self.rect = pygame.Rect(self.pos[0], self.pos[1], self.width, self.height)
+		self.y = (5 + self.height) * y + 5
+		self.pos = (self.width * x, self.height * y)		
+		self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 		
 		self.parent = None		
 
-		self.color = (126,126,255)
-
 		self.walkable = True
+	
+		self.dirty = False
+		white = (255,255,255)
+		self._color = white
+		
 		#position is x goes right
 		# y goes down so 
 		
 		self.f = None
 		self.g = None
 		self.h = None
-
-	def draw(self, screen, color):		
-		color = (0, 0, 255) if (self.walkable) else (255,0,0)
-		width = screen.get_size()[0]
-		self.rect[2] =  math.sqrt(width) * 2 
-		height = screen.get_size()[1]	
-		self.rect[3] =  math.sqrt(height) * 2
+		
+	@property
+	def color(self):
+		return self._color
+	
+	@color.setter
+	def color(self, value):
+		#print("set dirty")
+		self.dirty = True
+		self._color = value
+	
+	#properties	
+	
+	
+	def draw(self, screen, font):		
+		if not self.walkable: 
+			self._color = (255,0,0)
+		
 		#create some text to go on the fill
-		font = pygame.font.Font(None, 15)
+		
 		#info to display
-		info = str(self.x) + " , " + str(self.y)
+		info = str((self.x, self.y))
 		#render the text
-		text = font.render( info, 1, (1, 1, 1))
+		text = font.render(info, 1, (1, 1, 1))
 		#set it's position/parent
 		textpos = self.rect
 		#center it
 		textpos.centerx = self.rect.centerx
+		
+		
 		#draw the square
-		screen.fill(color,self.rect)
+		pygame.draw.rect(screen, self._color, self.rect)
+		#screen.fill(self._color, self.rect)
 		#blit the text 
 		screen.blit(text, textpos)
 		
-		
-		
-	def setWalk(self, walkable):
-		self.walkable = walkable
-		 
 	def getF(self):
 		return self.h + self.g
 	def setH(self, val):
 		self.h = val
 	def setG(self, val):
 		self.g = val
+	def onclick(self, pos):	
+		 
+		oldColor = self.color
+		newColor = (255,0,255)
+		#if we have set it's color manually then it is dirty so ignore it
+		if not self.dirty:			
+			x = pos[0]
+			y = pos[1]		
+			#if we click in this square set it's color
+			if(x > self.rect.left and x < self.rect.right and y >self.rect.top and y < self.rect.bottom):
+				print("clicked on square at ", self.rect)
+				self._color = newColor
+			#if this isn't the square we clicked then set it back to the original
+			else:
+				self._color = white
+		
+		
+		
+		
+		
 
 class Astar:
 	def __init__(self, SearchSpace, Start, Goal):
@@ -66,16 +101,10 @@ class Astar:
 	def Run(self):
 		self.OPEN.append(Start)
 		while not self.OPEN:
-			current = self.LowestF(self.OPEN)
-			
-	def LowestF(self, Nodes):
-		lowestF = -1
-		nodeWithLowestF = None
-		for node in Nodes:
-			if(node.f > lowestF):
-				lowestF = node.f
-				nodeWithLowestF = node
-		return nodeWithLowestF
+			current = self.LowestF(self.OPEN)			
+
+
+		
 '''
 	TODO.Add(start)
 
--- root/game.py
+++ root/game.py
@@ -17,33 +17,37 @@ def mag(a):
 	return math.sqrt(a[0]^2 + a[1]^2)
 	
 def main():
+	
 	Red = (255,255,255)
-	Grn = (0,255,0)        
+	Green = (0,255,0)     
+	Yellow = (255,255,0)	
 	print("mag of 255,255", mag((255,255)))
 	#create the search space to look through
-	searchSpace = []
-	for x in range(10):
-		for y in range(10):
+	mouse_listeners = []
+	search_space = []
+	for x in range(15):
+		for y in range(15):
 			n = Node(x, y)
 			#x goes right
 			#y goes down
-			unwalkable = True if (x >= 5 and x <= 6 and y >= 5 and y <= 8) else False
-			#print("x =:{mx} y=: {my} | pos =: {position}".format(mx = x, my = y, position = n.pos))
+			if (x >= 5 and x <= 6 and y >= 5 and y <= 8):
+				n.walkable = False
+				
+			mouse_listeners.append(n.onclick)
 			
-			n.setWalk(unwalkable)
-			searchSpace.append(n)
-			
+			search_space.append(n)
+		
 
 	# Initialize pygame
 	pygame.init()
 	# Initialize fonts
 	pygame.font.init()
 	# Set the HEIGHT and WIDTH of the screen
-	screen_width = 1000
-	screen_height = 1000
+	screen_width = 830
+	screen_height = 850
 	# Set the screen
 	screen = pygame.display.set_mode([screen_width, screen_height])
-
+	font = pygame.font.Font(None, 15)
 	# Set title of screen
 	pygame.display.set_caption("Astar")
 
@@ -51,25 +55,32 @@ def main():
 	# Used to manage how fast the screen updates
 	clock = pygame.time.Clock()
 	
-	 
+
 	Running = True
 	# -------- Main Program Loop -----------
-	while Running:
-		clicked = (0,0)
+	while Running:		
+		search_space[5].color = Green
 		for event in pygame.event.get():  # User did something			
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				clicked = pygame.mouse.get_pos()
+				for callback in mouse_listeners:
+					callback(pygame.mouse.get_pos())
+				
+				
 			if event.type == pygame.QUIT:
 				Running = False
 		
 		if(pygame.key.get_pressed()[pygame.K_ESCAPE]):
 			Running = False
 			
-		for i in searchSpace:
-			i.draw(screen, Red)
+		for i in search_space:
+			i.draw(screen, font)
+		print(clock.get_fps())
+		clock.tick(60)
+			
 		
 		# Go ahead and update the screen with what we've drawn.
 		pygame.display.flip()
+		
 
 	# Be IDLE friendly. If you forget this line, the program will 'hang'
 	# on exit.
